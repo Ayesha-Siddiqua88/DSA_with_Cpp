88dac6dbda53e3d08f46ebf4f51252f1b38d65dc
Ayesha Siddiqua <128472306+Ayesha-Siddiqua88@users.noreply.github.com>
2023-12-10 14:08:21 +0530

    Update Single_occur_num.cpp

--- Single_occur_num.cpp
+++ Single_occur_num.cpp
@@ -20,3 +20,22 @@ public:
     }
 };
 }
+
+// method 2
+// The question states that we have to find an element in the array with frequency=1.
+// So , the first idea that pops in the mind is to store the frequency of each element in a map (or a frequency array) and then traverse that map/array and return the element with frequency=1.
+
+// Map the given array's elements to their frequency. ( KEY : ELEMENT , VALUE : FREQUENCY )
+// Traverse that map and return the key whose value =1.
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       unordered_map<int,int> a;
+	   for(auto x: nums)
+		   a[x]++;
+	   for(auto z:a)
+		   if(z.second==1)
+			   return z.first;
+	   return -1;
+    }
+};
